08c79c75bd9f3110fc4e88db0a1bd220be80e12b
Timofey <kalinata0799@gmail.com>
2019-10-01 20:59:23 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guessNumber = this.max / 2;
     }
 
     guess() {
-
+        this.guessNumber = Math.round(this.min + (this.max - this.min)/2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
